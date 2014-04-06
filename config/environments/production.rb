30a797a317b31ebcb6052e6e1e65715c5303aa57
Mikhail Vaysman <9996113+github@gmail.com>
2014-04-06 05:15:11 +0400

    setup cascade. landing page added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Urbanity::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  config.action_mailer.delivery_method = :smtp
 end
