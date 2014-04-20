e0fbc86a7ded1c5cb9619387180ba57fe7005144
Mikhail Vaysman <9996113+github@gmail.com>
2014-04-20 04:10:43 +0400

    show games if user signed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_if_signed_in
+
   def index
     render layout: "welcome"
+  end
+
+  private
+
+  def redirect_if_signed_in
+    if user_signed_in?
+      redirect_to games_path
+    end
   end
 end
