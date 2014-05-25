ee74c8c1a124ff67dd47a627722ab6a20b3371cc
Mikhail Vaysman <9996113+github@gmail.com>
2014-05-26 03:25:53 +0400

    validation added. swap_position added.

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,4 +2,17 @@ class Quest < ActiveRecord::Base
   default_scope { order(:position) }
 
   belongs_to :game
+
+  validates :name, :game, presence: true
+  validates :position, numericality: { only_integer: true, greater_than: 0 }, uniqueness: { scope: :game }
+
+  def swap_position(other)
+    Quest.transaction do
+      pos = self.position
+      self.position = other.position
+      other.position = pos
+      other.save(validate: false)
+      save
+    end
+  end
 end
