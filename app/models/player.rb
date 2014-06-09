c9d918e5adb9c8320660319f5523ebcc7ad410a4
Mikhail Vaysman <9996113+github@gmail.com>
2014-06-09 23:50:57 +0400

    fix foreign key

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
 
-  has_many :games
+  has_many :games, foreign_key: :creator_id
   has_many :invitations
 
   validates :user, :nick, presence: true
