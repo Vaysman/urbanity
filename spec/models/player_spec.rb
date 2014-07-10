579de4da774a710da7c9a94886d562cb71fbf15e
Mikhail Vaysman <9996113+github@gmail.com>
2014-07-10 01:50:25 +0000

    own_team? spected

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,7 +10,27 @@ RSpec.describe Player, :type => :model do
   it { should validate_presence_of(:nick) }
   it { should validate_presence_of(:user) }
 
-  describe "#own_team?" do
-    # TODO
+  context "w/o team" do
+    let(:player) { Player.new }
+    describe "#own_team?" do
+      it "returns nil" do
+        player.own_team?.should be_nil
+        player.own_team?(Team.new).should be_nil
+      end
+    end
+  end
+
+  context "with team" do
+    let(:team) { Team.new }
+    let(:player) { Player.new(team: team) }
+    describe "#own_team?" do
+      it "returns false for other team" do
+        player.own_team?(Team.new).should be_false
+      end
+
+      it "returns true for own team" do
+        player.own_team?(team).should be_true
+      end
+    end
   end
 end
