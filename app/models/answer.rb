a9210d27b4a1cb2608671b6e7e154477729b23a0
Mikhail Vaysman <9996113+github@gmail.com>
2014-07-21 23:57:11 +0400

    fix typo

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :quest
 
-  validates :test, presence: true
+  validates :text, presence: true
   validates :points, numericality: { only_integer: true, greater_than: 0 }
 end
