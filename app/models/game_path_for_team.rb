7408917a78dfe0a889301b0547fc9bbe8129be19
Mikhail Vaysman <9996113+github@gmail.com>
2014-07-26 23:13:07 +0000

    add validation to game_path_for_team model

--- app/models/game_path_for_team.rb
+++ app/models/game_path_for_team.rb
@@ -2,4 +2,6 @@ class GamePathForTeam < ActiveRecord::Base
   belongs_to :game
   belongs_to :team
   belongs_to :quest
+
+  validates :position, numericality: { only_integer: true, greater_than: 0 }, uniqueness: { scope: [:game, :quest, :team] }
 end
