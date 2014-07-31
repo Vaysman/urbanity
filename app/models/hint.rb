c1197d2313be69c7deb1a20ee1c45fa4978d4242
Mikhail Vaysman <9996113+github@gmail.com>
2014-07-31 19:28:00 +0400

    fix hint

--- app/models/hint.rb
+++ app/models/hint.rb
@@ -1,3 +1,7 @@
 class Hint < ActiveRecord::Base
   belongs_to :quest
+
+  validates :text, presence: true
+  validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, uniqueness: { scope: :quest }
+  validates :delay, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
