9f7705f02ff5f40a9fb1db18b3fa86b74c85c8ca
Mikhail Vaysman <9996113+github@gmail.com>
2014-08-03 22:27:02 +0000

    spec swap_position

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Quest, :type => :model do
   it { should validate_presence_of(:game) }
   it { should validate_numericality_of(:position).only_integer.is_greater_than(0) }
   it { should validate_uniqueness_of(:position).scoped_to(:game_id) }
+
+  describe "#swap_position" do
+    it "swap position of two quest" do
+      quest1 = Quest.create!(position: 1)
+      quest2 = Quest.create!(position: 2)
+      quest1.swap_position(quest2)
+      expect(quest1.position).to eq(2)
+      expect(quest2.position).to eq(1)
+    end
+  end
 end
