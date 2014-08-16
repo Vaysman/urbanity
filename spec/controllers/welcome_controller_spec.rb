43fd46e2998518661b48c5b63c347cada6867a8f
Mikhail Vaysman <9996113+github@gmail.com>
2014-08-17 03:18:38 +0400

    finish welcome controller spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
-  context "user logged in" do
+  context "user not logged in" do
     before do
       sign_out :user
     end
@@ -10,6 +10,28 @@ RSpec.describe WelcomeController, :type => :controller do
       it "returns http success" do
         get :index
         expect(response).to be_success
+      end
+
+      it "renders index template" do
+        get :index
+        expect(response).to render_template(:index, layout: "welcome")
+      end
+    end
+  end
+
+  context "user logged in" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = User.new
+      user.confirmation_sent_at = Time.now
+      user.confirm!
+      sign_in :user, user
+    end
+
+    describe "GET index" do
+      it "redirect to games" do
+        get :index
+        expect(response).to redirect_to(games_path)
       end
     end
   end
