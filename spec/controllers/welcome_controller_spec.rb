cf0c4fc37d4f5bd633fb0ea159e02201bb635a32
Mikhail Vaysman <9996113+github@gmail.com>
2014-08-17 02:51:15 +0400

    fix controller spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
   context "user logged in" do
+    before do
+      sign_out :user
+    end
+
     describe "GET index" do
       it "returns http success" do
         get :index
