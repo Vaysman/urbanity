7a4f058484b457a3f1053a3941514f0f311a07b3
Mikhail Vaysman <9996113+github@gmail.com>
2014-08-25 00:06:40 +0400

    remove unnecessary comment

--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -1,15 +1,6 @@
 class PlayerDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
   def full_name
     if (first_name.blank? && last_name.blank?)
       "Nemo"
