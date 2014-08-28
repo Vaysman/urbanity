e3ca50f121640d7b44a8320417cae77a89d87038
Mikhail Vaysman <9996113+github@gmail.com>
2014-08-28 02:10:25 +0000

    finish team spec

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 RSpec.describe Team, :type => :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:captain) }
+
+  it { should belong_to(:captain) }
+  it { should have_many(:members) }
+  it { should have_many(:invitations) }
+  it { should have_many(:invitees) }
 end
