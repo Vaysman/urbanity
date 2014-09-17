b88722386befd26c15d7fc2955e29d4a4d922926
Mikhail Vaysman <9996113+github@gmail.com>
2014-09-17 23:20:45 +0000

    add geme decorator spec

---
+++ spec/decorators/game_decorator_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe GameDecorator do
+  describe "#start" do
+    it "returns nice formated date and time with icon" do
+      game = double(Game)
+      start_time = Time.new(2014,9,17,23,05,25)
+      allow(game).to receive(:start).and_return(start_time)
+      it = GameDecorator.new(game)
+      expect(it.start).to match(/\<span\s+class\s*=\s*"glyphicon glyphicon-time"> 17\.09\.2014 23:05<\/span>/)
+    end
+  end
+end
