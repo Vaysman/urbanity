37b76c3c431d93c1aaddb003cb7c6664101d8993
Mikhail Vaysman <9996113+github@gmail.com>
2014-09-18 00:37:08 +0000

    add more example to game decorator spec

--- spec/decorators/game_decorator_spec.rb
+++ spec/decorators/game_decorator_spec.rb
@@ -10,4 +10,14 @@ describe GameDecorator do
       expect(it.start).to match(/\<span\s+class\s*=\s*"glyphicon glyphicon-time"> 17\.09\.2014 23:05<\/span>/)
     end
   end
+
+  describe "#registration_end" do
+    it "returns nice formated date and time with icon" do
+      game = double(Game)
+      end_time = Time.new(2014,9,17,23,05,25)
+      allow(game).to receive(:registration_end).and_return(end_time)
+      it = GameDecorator.new(game)
+      expect(it.registration_end).to match(/\<span\s+class\s*=\s*"glyphicon glyphicon-time"> 17\.09\.2014 23:05<\/span>/)
+    end
+  end
 end
