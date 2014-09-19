7e05ccd42699647a339823b20fc6572902375298
Mikhail Vaysman <9996113+github@gmail.com>
2014-09-19 01:57:18 +0000

    add more example to game decorator spec

--- spec/decorators/game_decorator_spec.rb
+++ spec/decorators/game_decorator_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
 describe GameDecorator do
+  let(:game) { double(Game) }
+  let(:it) { GameDecorator.new(game) }
+
   describe "#start" do
     it "returns nice formated date and time with icon" do
-      game = double(Game)
       start_time = Time.new(2014,9,17,23,05,25)
       allow(game).to receive(:start).and_return(start_time)
-      it = GameDecorator.new(game)
       expect(it.start).to match(/\<span\s+class\s*=\s*"glyphicon glyphicon-time"> 17\.09\.2014 23:05<\/span>/)
     end
   end
 
   describe "#registration_end" do
     it "returns nice formated date and time with icon" do
-      game = double(Game)
       end_time = Time.new(2014,9,17,23,05,25)
       allow(game).to receive(:registration_end).and_return(end_time)
-      it = GameDecorator.new(game)
       expect(it.registration_end).to match(/\<span\s+class\s*=\s*"glyphicon glyphicon-time"> 17\.09\.2014 23:05<\/span>/)
+    end
+  end
+
+  describe "#registred_teams_count" do
+    it "returns 5" do
+      expect(it.registred_team_count).to eq(5)
     end
   end
 end
