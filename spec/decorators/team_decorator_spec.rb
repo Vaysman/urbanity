cac15f1d59121b91a15b2ce96499bc2d50ff7da7
Mikhail Vaysman <9996113+github@gmail.com>
2014-09-21 00:20:03 +0000

    add team decorator spec

---
+++ spec/decorators/team_decorator_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe TeamDecorator do
+  let(:team) { double(Team) }
+  let(:it) { TeamDecorator.new(team) }
+
+  describe "#size" do
+    allow(team).to receive(:members).and_return(Array.new(1))
+
+    expect(it.size).to eq(1)
+  end
+end
