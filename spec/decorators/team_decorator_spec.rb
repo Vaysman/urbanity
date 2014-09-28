59c203b236b6f8c1f957a5b46d9d2f7fd69dc42e
Mikhail Vaysman <9996113+github@gmail.com>
2014-09-29 00:11:17 +0400

    finish team decorator spec

--- spec/decorators/team_decorator_spec.rb
+++ spec/decorators/team_decorator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe TeamDecorator do
   include Rails.application.routes.url_helpers
-  
+
   let(:team) { FactoryGirl.create(:team) }
   let(:it) { TeamDecorator.new(team) }
 
@@ -18,13 +18,18 @@ describe TeamDecorator do
     let(:player) { double(Player) }
 
     context "player own team" do
+      it "returns html fragmet with show link and edit link" do
+        allow(player).to receive("own_team?").and_return(true)
+        expect(it.actions_for(player)).to match(/>Show<\/a>/)
+        expect(it.actions_for(player)).to match(/>Edit<\/a>/)
+      end
     end
 
     context "player doesn't own team" do
       it "returns html fragmet with show link" do
-        it = FactoryGirl.create(:team).decorate
         allow(player).to receive("own_team?").and_return(false)
         expect(it.actions_for(player)).to match(/>Show<\/a>/)
+        expect(it.actions_for(player)).to_not match(/>Edit<\/a>/)
       end
     end
   end
